001d9b10a1891cf70cb215a61bfc9ebbe62a1f76
birdman98 <piotrmatras21@gmail.com>
2018-12-15 18:22:00 +0100

    added some validation classes with validation methods

--- Validators.cpp
+++ Validators.cpp
@@ -2,33 +2,47 @@
 
 #include <regex>
 
-
-bool IDData::validate(const std::string &toCheck) {
-
-	std::regex pattern("([A-Z][a-z]+ ){2}");
-
-	return std::regex_match(toCheck, pattern);
-
-}
-	 
-
-bool Pesel::validate(const std::string &toCheck) {
+bool ComplaintTitle::validate(const std::string &toCheck) {
 	
-	std::regex pattern("([0-9]{11}");
+	std::regex pattern(".+");
 
 	return std::regex_match(toCheck, pattern);
 }
 
-bool Adress::validate(const std::string &toCheck) {
+bool ComplaintedItem::validate(const std::string &toCheck) {
 	
-	std::regex pattern("[A-Z][a-z]+ [0-9]+[A-Za-z]+///[0-9]+ [0-9]{2}-[0-9]{3}"); //rozbiæ adres na kilka pól lub na oddzieln¹ klasê i porobiæ walidatory do pojedynczych pól
+	std::regex pattern("(\\w| )+");
+
+	return std::regex_match(toCheck, pattern);
+}
+
+bool IDData::validate(const std::string &toCheck) { 
+
+	std::regex pattern("(([A-Z][a-z]+)| )+");
+
+	return std::regex_match(toCheck, pattern);
+
+}
+
+bool Adress::validate(const std::string &toCheck) { 
+
+	std::regex pattern("[A-Z][a-z]+ [0-9]*[A-Za-z]*/*[0-9]* *[0-9]{2}-[0-9]{3} [A-Z][a-z]+");
 
 	return  std::regex_match(toCheck, pattern);
 }
 
 bool PhoneNumber::validate(const std::string &toCheck) {
-	
+
 	std::regex pattern("[1-9][0-9]{8}");
 
 	return  std::regex_match(toCheck, pattern);
 }
+
+bool Pesel::validate(const std::string &toCheck) {
+	
+	std::regex pattern("[0-9]{11}");
+
+	return std::regex_match(toCheck, pattern);
+}
+
+
